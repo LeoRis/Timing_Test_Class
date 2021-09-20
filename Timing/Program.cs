05add279545f30504a7449e39e0d8b9c561f723b
Leo Ris <ristanovski@hotmail.com>
2021-09-20 08:51:57 +0200

    Creating timing test class.

---
+++ Timing/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Timing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sizeOfTheArray;
+            Console.WriteLine("Plesae insert array size: ");
+
+            sizeOfTheArray = Convert.ToInt32(Console.ReadLine());
+
+            int[] nums = new int[sizeOfTheArray];
+            BuildArray(nums, sizeOfTheArray);
+            TimeSpan startTime;
+            TimeSpan duration;
+
+            startTime = Process.GetCurrentProcess().Threads[0].UserProcessorTime;
+
+            DisplayNums(nums);
+
+            duration = Process.GetCurrentProcess().Threads[0].UserProcessorTime.Subtract(startTime);
+
+            Console.WriteLine("Time: " + duration.TotalSeconds);
+
+            Console.ReadLine();
+        }
+
+        public static void BuildArray(int[] array, int size)
+        {
+            for(int i = 0; i <= size; i++)
+            {
+                array[i] = i;
+            }
+        }
+
+        public static void DisplayNums(int[] array)
+        {
+            for(int i = 0; i < array.GetUpperBound(0); i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+        }
+    }
+}
